695c63d06d63edd824c946379e78c6f30667854a
pankaj2k9 <pkp2.me2k9@gmail.com>
2023-05-28 00:55:14 +0600

    Done animations

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -7,39 +11,12 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Axes Helper
+ * Base
  */
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
-
-/**
- * Objects
- */
-const group = new THREE.Group()
-group.scale.y = 2
-group.rotation.y = 0.2
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-cube1.position.x = - 1.5
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-cube2.position.x = 0
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-cube3.position.x = 1.5
-group.add(cube3)
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 /**
  * Sizes
@@ -54,7 +31,6 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-// camera.lookAt(new THREE.Vector3(0, - 1, 0))
 scene.add(camera)
 
 /**
@@ -64,4 +40,19 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+/**
+ * Animate
+ */
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+
+const tick = () =>
+{
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
