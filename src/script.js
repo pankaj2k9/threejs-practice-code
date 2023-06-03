41f10a0f483617cd6243b8b4d647332be6d52147
pankaj2k9 <pkp2.me2k9@gmail.com>
2023-06-03 22:28:19 +0600

    added name

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Pankaj Pramanik',
             {
                 font: font,
                 size: 0.5,
